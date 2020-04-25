8cff62e8c1935db0494d01929d71670461db58d7
Ahmed Sattar <sobiddya@gmail.com>
2020-04-25 15:16:56 +0600

    quick save

--- c.c
+++ c.c
@@ -1,31 +1,12 @@
 #include <stdio.h>
-
-typedef unsigned long long int bigint;
-bigint maxnum(char* s);
-
+#include <stdlib.h>
+#include <string.h>
+//Hello hello
+#define MAX 700
 int main (int argc, char *argv[]){
-	
-	unsigned int i=0;
-	unsigned int j=1;
-	//while(j>i){
-	//	i++;
-	//	j++;
-	//}
-	int k = sizeof(i);
-	bigint l=maxnum("unsigned int");
-	bigint ul=(l/2);
-	//printf ("%d bytes: %lu...%lu \n",k,j,i);
-	printf ("unsigned: 0...%llu\nsigned: -%llu...%llu",(l-1),ul,ul-1);
-	//int x=2147483648;
-	//printf ("%d",x);
+	char b=(0b110001000);
+	printf ("%d",b>>2); EOF
 	return 0;
+	TRUE
 }
-bigint maxnum(char *s){
-	bigint ans=1;
-	int n=sizeof(s);
-	int i;
-	for (i=0 ; i<n ; i+=1){
-		ans = ans*256;	
-	}
-	return ans;
-}
+0 10000000 11000000000000000000000
