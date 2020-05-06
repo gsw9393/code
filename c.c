e36098265e619d3933ade780ddad04f2a6f168d8
Ahmed Sattar <sobiddya@gmail.com>
2020-05-06 15:46:27 +0600

    quicksave

--- c.c
+++ c.c
@@ -10,13 +10,31 @@ void initQueue (Queue *q){
     q->tail=0;
 }
 // insert a new item at the tail of the Queue
+
 void enterQueue (Queue *q, int item){
+    if (q->nitems!=0){//checks if tail is end of array
+        q->tail=(q->tail+1)%MAXQ;//if end of 
+    }
     q->items[q->tail]=item;
-    (q->tail)++;
+    q->tail++;
 }
 // remove/return the item at the head of the Queue
-int leaveQueue (Queue *);
+int leaveQueue (Queue *q){
+    int temp = q->items[q->head];
+    q->head++;
+    return temp;
+}
 // return the number of items currently in the Queue
 int lengthQueue (Queue);
 // display the contents of the Queue
 void showQueue (Queue);
+
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[]){
+    int c=6%6;
+    printf ("%d",c);
+    return 0;
+}
